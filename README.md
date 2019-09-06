e0694ba01cbef442e7e81bd1c0106de297e6c761
Thejas007 <thejasgowda.n@gmail.com>
2019-09-06 14:51:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# clarity-dark-theme-v2-ddcpzt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/clarity-dark-theme-v2-ddcpzt)
---
+++ src/app/users.ts
@@ -0,0 +1,601 @@
+export const USERS = [{
+    "id": 51434,
+    "name": "Marcella",
+    "creation": "2015-02-07T10:35:18.994Z",
+    "color": "Purple",
+    "pokemon": {"number": 34, "name": "Nidoking"}
+}, {
+    "id": 30383,
+    "name": "Marcella",
+    "creation": "2016-06-19T07:19:59.152Z",
+    "color": "Orange",
+    "pokemon": {"number": 39, "name": "Jigglypuff"}
+}, {
+    "id": 92892,
+    "name": "Olene",
+    "creation": "2016-04-17T04:59:02.715Z",
+    "color": "White",
+    "pokemon": {"number": 110, "name": "Weezing"}
+}, {
+    "id": 80541,
+    "name": "Nidia",
+    "creation": "2016-06-20T21:57:49.085Z",
+    "color": "Green",
+    "pokemon": {"number": 36, "name": "Clefable"}
+}, {
+    "id": 42604,
+    "name": "Dorian",
+    "creation": "2014-11-19T19:36:26.270Z",
+    "color": "Black",
+    "pokemon": {"number": 14, "name": "Kakuna"}
+}, {
+    "id": 17096,
+    "name": "Roslyn",
+    "creation": "2015-04-21T23:34:45.507Z",
+    "color": "Blue",
+    "pokemon": {"number": 35, "name": "Clefairy"}
+}, {
+    "id": 23015,
+    "name": "Keenan",
+    "creation": "2014-07-04T18:50:56.451Z",
+    "color": "Blue",
+    "pokemon": {"number": 124, "name": "Jynx"}
+}, {
+    "id": 12472,
+    "name": "Sibyl",
+    "creation": "2015-10-23T15:02:11.411Z",
+    "color": "Indigo",
+    "pokemon": {"number": 69, "name": "Bellsprout"}
+}, {
+    "id": 46249,
+    "name": "Sibyl",
+    "creation": "2017-08-11T05:04:06.313Z",
+    "color": "Indigo",
+    "pokemon": {"number": 136, "name": "Flareon"}
+}, {
+    "id": 97156,
+    "name": "Marcella",
+    "creation": "2015-04-06T23:40:56.169Z",
+    "color": "Indigo",
+    "pokemon": {"number": 84, "name": "Doduo"}
+}, {
+    "id": 31399,
+    "name": "Darla",
+    "creation": "2016-09-25T13:58:41.431Z",
+    "color": "Black",
+    "pokemon": {"number": 33, "name": "Nidorino"}
+}, {
+    "id": 59673,
+    "name": "Luna",
+    "creation": "2016-12-22T20:52:41.707Z",
+    "color": "Gray",
+    "pokemon": {"number": 60, "name": "Poliwag"}
+}, {
+    "id": 9595,
+    "name": "Marcella",
+    "creation": "2014-08-06T12:11:49.317Z",
+    "color": "Purple",
+    "pokemon": {"number": 140, "name": "Kabuto"}
+}, {
+    "id": 73929,
+    "name": "Keenan",
+    "creation": "2014-12-17T17:06:11.793Z",
+    "color": "Purple",
+    "pokemon": {"number": 2, "name": "Ivysaur"}
+}, {
+    "id": 62530,
+    "name": "Marcella",
+    "creation": "2017-09-17T06:54:29.743Z",
+    "color": "Gray",
+    "pokemon": {"number": 71, "name": "Victreebel"}
+}, {
+    "id": 98585,
+    "name": "Nidia",
+    "creation": "2014-02-04T12:27:34.257Z",
+    "color": "Orange",
+    "pokemon": {"number": 114, "name": "Tangela"}
+}, {
+    "id": 40208,
+    "name": "Lucius",
+    "creation": "2017-01-18T07:01:50.904Z",
+    "color": "Green",
+    "pokemon": {"number": 11, "name": "Metapod"}
+}, {
+    "id": 22332,
+    "name": "Desirae",
+    "creation": "2015-02-22T02:27:06.187Z",
+    "color": "Red",
+    "pokemon": {"number": 86, "name": "Seel"}
+}, {
+    "id": 41483,
+    "name": "Dorian",
+    "creation": "2014-02-28T18:12:16.814Z",
+    "color": "Violet",
+    "pokemon": {"number": 61, "name": "Poliwhirl"}
+}, {
+    "id": 66856,
+    "name": "Sheridan",
+    "creation": "2017-10-12T13:31:37.805Z",
+    "color": "Magenta",
+    "pokemon": {"number": 34, "name": "Nidoking"}
+}, {
+    "id": 78094,
+    "name": "Dorian",
+    "creation": "2017-12-11T21:25:34.947Z",
+    "color": "Black",
+    "pokemon": {"number": 128, "name": "Tauros"}
+}, {
+    "id": 64795,
+    "name": "Marcella",
+    "creation": "2016-02-06T00:03:01.563Z",
+    "color": "Green",
+    "pokemon": {"number": 106, "name": "Hitmonlee"}
+}, {
+    "id": 85921,
+    "name": "Debby",
+    "creation": "2014-03-08T03:34:26.911Z",
+    "color": "Orange",
+    "pokemon": {"number": 111, "name": "Rhyhorn"}
+}, {
+    "id": 68118,
+    "name": "Graham",
+    "creation": "2017-03-13T18:22:42.073Z",
+    "color": "Green",
+    "pokemon": {"number": 43, "name": "Oddish"}
+}, {
+    "id": 13651,
+    "name": "Roslyn",
+    "creation": "2014-10-31T20:01:36.631Z",
+    "color": "Black",
+    "pokemon": {"number": 43, "name": "Oddish"}
+}, {
+    "id": 38420,
+    "name": "Keenan",
+    "creation": "2016-02-05T20:09:22.200Z",
+    "color": "Pink",
+    "pokemon": {"number": 135, "name": "Jolteon"}
+}, {
+    "id": 14944,
+    "name": "Sheridan",
+    "creation": "2015-11-21T16:37:11.335Z",
+    "color": "Red",
+    "pokemon": {"number": 66, "name": "Machop"}
+}, {
+    "id": 39511,
+    "name": "Letisha",
+    "creation": "2015-07-17T18:58:21.360Z",
+    "color": "White",
+    "pokemon": {"number": 34, "name": "Nidoking"}
+}, {
+    "id": 6731,
+    "name": "Marcella",
+    "creation": "2014-12-06T05:25:58.070Z",
+    "color": "Green",
+    "pokemon": {"number": 57, "name": "Primeape"}
+}, {
+    "id": 40230,
+    "name": "Alica",
+    "creation": "2016-08-01T21:40:18.966Z",
+    "color": "Indigo",
+    "pokemon": {"number": 149, "name": "Dragonite"}
+}, {
+    "id": 35786,
+    "name": "Shenika",
+    "creation": "2017-06-22T22:42:45.366Z",
+    "color": "Pink",
+    "pokemon": {"number": 8, "name": "Wartortle"}
+}, {
+    "id": 25598,
+    "name": "Olene",
+    "creation": "2014-04-22T10:52:14.689Z",
+    "color": "Cyan",
+    "pokemon": {"number": 51, "name": "Dugtrio"}
+}, {
+    "id": 65736,
+    "name": "Genoveva",
+    "creation": "2016-10-07T07:01:53.144Z",
+    "color": "Black",
+    "pokemon": {"number": 42, "name": "Golbat"}
+}, {
+    "id": 41264,
+    "name": "Nelson",
+    "creation": "2015-09-24T03:53:13.825Z",
+    "color": "Gray",
+    "pokemon": {"number": 127, "name": "Pinsir"}
+}, {
+    "id": 66164,
+    "name": "Nelson",
+    "creation": "2014-04-24T03:59:00.811Z",
+    "color": "Purple",
+    "pokemon": {"number": 55, "name": "Golduck"}
+}, {
+    "id": 20886,
+    "name": "Roslyn",
+    "creation": "2015-03-31T22:31:09.321Z",
+    "color": "White",
+    "pokemon": {"number": 70, "name": "Weepinbell"}
+}, {
+    "id": 43754,
+    "name": "Shenika",
+    "creation": "2015-07-16T14:00:38.128Z",
+    "color": "Brown",
+    "pokemon": {"number": 46, "name": "Paras"}
+}, {
+    "id": 18635,
+    "name": "Lenny",
+    "creation": "2016-04-12T09:13:01.861Z",
+    "color": "Green",
+    "pokemon": {"number": 115, "name": "Kangaskhan"}
+}, {
+    "id": 38721,
+    "name": "Rhona",
+    "creation": "2017-06-07T19:16:38.949Z",
+    "color": "Purple",
+    "pokemon": {"number": 113, "name": "Chansey"}
+}, {
+    "id": 85187,
+    "name": "Georgia",
+    "creation": "2017-05-26T07:05:34.485Z",
+    "color": "Red",
+    "pokemon": {"number": 48, "name": "Venonat"}
+}, {
+    "id": 83460,
+    "name": "Luna",
+    "creation": "2017-08-24T11:58:04.132Z",
+    "color": "Gray",
+    "pokemon": {"number": 143, "name": "Snorlax"}
+}, {
+    "id": 14501,
+    "name": "Lenny",
+    "creation": "2014-01-03T19:24:16.807Z",
+    "color": "Red",
+    "pokemon": {"number": 17, "name": "Pidgeotto"}
+}, {
+    "id": 47069,
+    "name": "Luna",
+    "creation": "2016-10-05T22:05:45.200Z",
+    "color": "Brown",
+    "pokemon": {"number": 21, "name": "Spearow"}
+}, {
+    "id": 41378,
+    "name": "Letisha",
+    "creation": "2014-04-03T07:55:25.439Z",
+    "color": "Violet",
+    "pokemon": {"number": 10, "name": "Caterpie"}
+}, {
+    "id": 49823,
+    "name": "Shenika",
+    "creation": "2016-12-02T12:26:09.523Z",
+    "color": "Indigo",
+    "pokemon": {"number": 16, "name": "Pidgey"}
+}, {
+    "id": 46101,
+    "name": "Shenika",
+    "creation": "2015-04-10T08:45:36.786Z",
+    "color": "Green",
+    "pokemon": {"number": 109, "name": "Koffing"}
+}, {
+    "id": 16383,
+    "name": "Johnson",
+    "creation": "2017-05-02T08:33:43.689Z",
+    "color": "Magenta",
+    "pokemon": {"number": 54, "name": "Psyduck"}
+}, {
+    "id": 14025,
+    "name": "Jeana",
+    "creation": "2017-09-18T03:09:11.016Z",
+    "color": "Red",
+    "pokemon": {"number": 77, "name": "Ponyta"}
+}, {
+    "id": 29312,
+    "name": "Debby",
+    "creation": "2017-12-27T10:10:20.391Z",
+    "color": "Cyan",
+    "pokemon": {"number": 40, "name": "Wigglytuff"}
+}, {
+    "id": 73506,
+    "name": "Shenika",
+    "creation": "2017-05-14T05:56:58.639Z",
+    "color": "Indigo",
+    "pokemon": {"number": 89, "name": "Muk"}
+}, {
+    "id": 20612,
+    "name": "Brynn",
+    "creation": "2014-10-17T19:01:03.823Z",
+    "color": "Magenta",
+    "pokemon": {"number": 2, "name": "Ivysaur"}
+}, {
+    "id": 21155,
+    "name": "Keenan",
+    "creation": "2018-01-31T16:42:06.226Z",
+    "color": "Pink",
+    "pokemon": {"number": 60, "name": "Poliwag"}
+}, {
+    "id": 55584,
+    "name": "Sibyl",
+    "creation": "2015-01-12T10:46:39.709Z",
+    "color": "Violet",
+    "pokemon": {"number": 147, "name": "Dratini"}
+}, {
+    "id": 62678,
+    "name": "Nelson",
+    "creation": "2016-04-24T04:31:31.726Z",
+    "color": "Gray",
+    "pokemon": {"number": 94, "name": "Gengar"}
+}, {
+    "id": 26382,
+    "name": "Brynn",
+    "creation": "2017-07-17T16:34:40.822Z",
+    "color": "Brown",
+    "pokemon": {"number": 151, "name": "Mew"}
+}, {
+    "id": 15248,
+    "name": "Beverly",
+    "creation": "2015-12-11T21:50:26.155Z",
+    "color": "Red",
+    "pokemon": {"number": 138, "name": "Omanyte"}
+}, {
+    "id": 69363,
+    "name": "Dorian",
+    "creation": "2014-09-27T07:42:21.760Z",
+    "color": "Green",
+    "pokemon": {"number": 44, "name": "Gloom"}
+}, {
+    "id": 84857,
+    "name": "Kaitlin",
+    "creation": "2017-09-12T09:35:14.090Z",
+    "color": "Pink",
+    "pokemon": {"number": 17, "name": "Pidgeotto"}
+}, {
+    "id": 11494,
+    "name": "Marcella",
+    "creation": "2014-01-18T11:13:16.033Z",
+    "color": "Cyan",
+    "pokemon": {"number": 81, "name": "Magnemite"}
+}, {
+    "id": 39131,
+    "name": "Brad",
+    "creation": "2015-07-26T17:29:42.198Z",
+    "color": "Brown",
+    "pokemon": {"number": 98, "name": "Krabby"}
+}, {
+    "id": 418,
+    "name": "Brad",
+    "creation": "2014-03-28T13:25:01.185Z",
+    "color": "Violet",
+    "pokemon": {"number": 78, "name": "Rapidash"}
+}, {
+    "id": 93073,
+    "name": "Brad",
+    "creation": "2016-12-07T11:21:11.196Z",
+    "color": "Magenta",
+    "pokemon": {"number": 137, "name": "Porygon"}
+}, {
+    "id": 9142,
+    "name": "Ellen",
+    "creation": "2014-01-20T21:31:34.433Z",
+    "color": "Pink",
+    "pokemon": {"number": 35, "name": "Clefairy"}
+}, {
+    "id": 14675,
+    "name": "Luna",
+    "creation": "2016-03-11T09:01:55.571Z",
+    "color": "Magenta",
+    "pokemon": {"number": 65, "name": "Alakazam"}
+}, {
+    "id": 74065,
+    "name": "Alica",
+    "creation": "2014-11-16T15:57:13.170Z",
+    "color": "Gray",
+    "pokemon": {"number": 84, "name": "Doduo"}
+}, {
+    "id": 30282,
+    "name": "Desirae",
+    "creation": "2014-03-16T23:05:40.729Z",
+    "color": "White",
+    "pokemon": {"number": 24, "name": "Arbok"}
+}, {
+    "id": 68857,
+    "name": "Luna",
+    "creation": "2014-07-24T03:44:38.242Z",
+    "color": "Cyan",
+    "pokemon": {"number": 103, "name": "Exeggutor"}
+}, {
+    "id": 35856,
+    "name": "Keenan",
+    "creation": "2016-10-29T09:18:41.043Z",
+    "color": "Pink",
+    "pokemon": {"number": 41, "name": "Zubat"}
+}, {
+    "id": 82364,
+    "name": "Kaitlin",
+    "creation": "2017-07-21T10:13:17.940Z",
+    "color": "Red",
+    "pokemon": {"number": 32, "name": "Nidoran-m"}
+}, {
+    "id": 30027,
+    "name": "Olene",
+    "creation": "2015-05-12T13:58:09.643Z",
+    "color": "Violet",
+    "pokemon": {"number": 1, "name": "Bulbasaur"}
+}, {
+    "id": 88625,
+    "name": "Sheridan",
+    "creation": "2015-08-25T08:01:54.993Z",
+    "color": "Black",
+    "pokemon": {"number": 69, "name": "Bellsprout"}
+}, {
+    "id": 25911,
+    "name": "Ellen",
+    "creation": "2014-02-15T00:34:23.484Z",
+    "color": "White",
+    "pokemon": {"number": 103, "name": "Exeggutor"}
+}, {
+    "id": 27439,
+    "name": "Sheridan",
+    "creation": "2017-11-13T02:53:39.722Z",
+    "color": "Brown",
+    "pokemon": {"number": 101, "name": "Electrode"}
+}, {
+    "id": 18711,
+    "name": "Jeana",
+    "creation": "2014-12-29T06:19:52.978Z",
+    "color": "Magenta",
+    "pokemon": {"number": 56, "name": "Mankey"}
+}, {
+    "id": 4300,
+    "name": "Dorian",
+    "creation": "2015-09-26T01:35:12.616Z",
+    "color": "Yellow",
+    "pokemon": {"number": 149, "name": "Dragonite"}
+}, {
+    "id": 41042,
+    "name": "Nelson",
+    "creation": "2015-12-28T14:41:07.429Z",
+    "color": "Yellow",
+    "pokemon": {"number": 32, "name": "Nidoran-m"}
+}, {
+    "id": 92645,
+    "name": "Ellen",
+    "creation": "2014-02-16T02:00:44.985Z",
+    "color": "Pink",
+    "pokemon": {"number": 98, "name": "Krabby"}
+}, {
+    "id": 50955,
+    "name": "Lenny",
+    "creation": "2015-10-05T05:59:09.814Z",
+    "color": "Pink",
+    "pokemon": {"number": 5, "name": "Charmeleon"}
+}, {
+    "id": 3224,
+    "name": "Brad",
+    "creation": "2014-05-12T15:31:40.310Z",
+    "color": "White",
+    "pokemon": {"number": 114, "name": "Tangela"}
+}, {
+    "id": 16332,
+    "name": "Lucius",
+    "creation": "2017-08-03T14:39:13.987Z",
+    "color": "Green",
+    "pokemon": {"number": 134, "name": "Vaporeon"}
+}, {
+    "id": 64184,
+    "name": "Lucius",
+    "creation": "2015-12-18T20:42:57.470Z",
+    "color": "White",
+    "pokemon": {"number": 57, "name": "Primeape"}
+}, {
+    "id": 87522,
+    "name": "Johnson",
+    "creation": "2017-10-30T12:06:39.501Z",
+    "color": "Cyan",
+    "pokemon": {"number": 139, "name": "Omastar"}
+}, {
+    "id": 69924,
+    "name": "Genoveva",
+    "creation": "2015-09-09T09:50:05.312Z",
+    "color": "Indigo",
+    "pokemon": {"number": 94, "name": "Gengar"}
+}, {
+    "id": 31207,
+    "name": "Jeana",
+    "creation": "2017-05-16T23:43:28.309Z",
+    "color": "Violet",
+    "pokemon": {"number": 30, "name": "Nidorina"}
+}, {
+    "id": 84467,
+    "name": "Desirae",
+    "creation": "2014-06-09T05:14:13.500Z",
+    "color": "Indigo",
+    "pokemon": {"number": 71, "name": "Victreebel"}
+}, {
+    "id": 62874,
+    "name": "Ellen",
+    "creation": "2014-02-15T16:35:25.080Z",
+    "color": "Magenta",
+    "pokemon": {"number": 70, "name": "Weepinbell"}
+}, {
+    "id": 26681,
+    "name": "Olene",
+    "creation": "2018-03-25T04:03:22.212Z",
+    "color": "Violet",
+    "pokemon": {"number": 142, "name": "Aerodactyl"}
+}, {
+    "id": 64033,
+    "name": "Jeana",
+    "creation": "2015-01-12T04:42:23.200Z",
+    "color": "Red",
+    "pokemon": {"number": 98, "name": "Krabby"}
+}, {
+    "id": 25459,
+    "name": "Sheridan",
+    "creation": "2016-01-04T02:34:52.892Z",
+    "color": "Cyan",
+    "pokemon": {"number": 130, "name": "Gyarados"}
+}, {
+    "id": 4890,
+    "name": "Beverly",
+    "creation": "2014-03-07T18:23:12.550Z",
+    "color": "Magenta",
+    "pokemon": {"number": 141, "name": "Kabutops"}
+}, {
+    "id": 78949,
+    "name": "Ellen",
+    "creation": "2016-08-20T19:01:07.974Z",
+    "color": "Brown",
+    "pokemon": {"number": 14, "name": "Kakuna"}
+}, {
+    "id": 36075,
+    "name": "Desirae",
+    "creation": "2015-11-30T14:04:51.388Z",
+    "color": "Black",
+    "pokemon": {"number": 95, "name": "Onix"}
+}, {
+    "id": 66121,
+    "name": "Luna",
+    "creation": "2017-05-20T22:14:50.272Z",
+    "color": "Red",
+    "pokemon": {"number": 148, "name": "Dragonair"}
+}, {
+    "id": 92829,
+    "name": "Kaitlin",
+    "creation": "2015-02-04T06:46:31.269Z",
+    "color": "Magenta",
+    "pokemon": {"number": 90, "name": "Shellder"}
+}, {
+    "id": 96241,
+    "name": "Luna",
+    "creation": "2018-01-26T17:11:59.900Z",
+    "color": "Purple",
+    "pokemon": {"number": 101, "name": "Electrode"}
+}, {
+    "id": 44781,
+    "name": "Olene",
+    "creation": "2018-01-23T19:01:33.026Z",
+    "color": "Magenta",
+    "pokemon": {"number": 65, "name": "Alakazam"}
+}, {
+    "id": 1262,
+    "name": "Brad",
+    "creation": "2017-10-14T21:49:09.867Z",
+    "color": "Violet",
+    "pokemon": {"number": 57, "name": "Primeape"}
+}, {
+    "id": 31002,
+    "name": "Keenan",
+    "creation": "2017-02-19T00:52:26.212Z",
+    "color": "Black",
+    "pokemon": {"number": 115, "name": "Kangaskhan"}
+}, {
+    "id": 48223,
+    "name": "Letisha",
+    "creation": "2016-09-15T08:16:52.613Z",
+    "color": "Cyan",
+    "pokemon": {"number": 90, "name": "Shellder"}
+}, {
+    "id": 87656,
+    "name": "Johnson",
+    "creation": "2015-06-23T12:50:54.383Z",
+    "color": "Purple",
+    "pokemon": {"number": 84, "name": "Doduo"}
+}];
